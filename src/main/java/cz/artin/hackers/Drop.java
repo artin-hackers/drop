c7eefb1e73cfc2a036888ca8c10f61d83fda6d43
zdenek-nemec <zdenek.nemec@artin.cz>
2019-04-07 09:45:16 +0200

    #1: Test (spawnChicken)

--- src/main/java/cz/artin/hackers/Drop.java
+++ src/main/java/cz/artin/hackers/Drop.java
@@ -1,7 +1,10 @@
 package cz.artin.hackers;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Drop extends JavaPlugin {
@@ -13,6 +16,16 @@ public class Drop extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("spawnChicken")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                Location location = player.getLocation();
+                location.add(5, 0, 0);
+                Chicken chicken = player.getWorld().spawn(location, Chicken.class);
+                return true;
+            }
+            return false;
+        }
 
         return false;
     }
