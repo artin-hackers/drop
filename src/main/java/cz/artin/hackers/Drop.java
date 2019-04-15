58b3d6d5a02c7b151d8f81e875e2bac8e0e9444d
Zdeněk Němec <33548481+zdenek-nemec@users.noreply.github.com>
2019-04-15 20:46:10 +0200

    #10: Creative mode by default (#11)
    * #10: Set creative mode
    
    * #10: Player-joined listener
    
    * #10: Creative mode by default for newly joined players
    
    * #10: Switch to normal mode

--- src/main/java/cz/artin/hackers/Drop.java
+++ src/main/java/cz/artin/hackers/Drop.java
@@ -1,22 +1,32 @@
 package cz.artin.hackers;
 
-import org.bukkit.Location;
+import org.bukkit.GameMode;
+import org.bukkit.GameRule;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Drop extends JavaPlugin {
-
+public class Drop extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getLogger().info("Loading DROP plugin...");
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("...plugin successfully loaded.");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (label.equalsIgnoreCase("spawnChicken")) {
+        if (label.equalsIgnoreCase("setModeDeveloper")) {
+            return setModeDeveloper(sender);
+        }
+        else if (label.equalsIgnoreCase("setModeNormal")) {
+            return setModeNormal(sender);
+        }
+        else if (label.equalsIgnoreCase("spawnChicken")) {
             return spawnChicken(sender);
         }
         else if (label.equalsIgnoreCase("spawnRabbit")) {
@@ -25,8 +35,30 @@ public class Drop extends JavaPlugin {
         else if (label.equalsIgnoreCase("spawnWolf")) {
             return spawnWolf(sender);
         }
-
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        getLogger().info("A new player, " + event.getPlayer().getName() + ", just joined the fray.");
+        event.getPlayer().setGameMode(GameMode.CREATIVE);
+    }
+
+    private boolean setModeDeveloper(CommandSender sender) {
+        if (sender instanceof Player) {
+            ((Player) sender).setGameMode(GameMode.CREATIVE);
+            ((Player) sender).getWorld().setTime(0);
+            ((Player) sender).getWorld().setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+        }
+        return true;
+    }
+
+    private boolean setModeNormal(CommandSender sender) {
+        if (sender instanceof Player) {
+            ((Player) sender).setGameMode(GameMode.SURVIVAL);
+            ((Player) sender).getWorld().setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true);
+        }
+        return true;
     }
 
     private boolean spawnChicken(CommandSender sender) {
