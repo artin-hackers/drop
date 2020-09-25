532059068026297f6771d96c528d68ea415e3f25
zdenek-nemec <zdenek.nemec@artin.cz>
2020-09-25 22:39:31 +0200

    #59: Code cleanup

--- src/main/java/cz/artin/hackers/ZireaelSword.java
+++ src/main/java/cz/artin/hackers/ZireaelSword.java
@@ -4,44 +4,27 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class ZireaelSword extends Item implements Listener {
-    // TODO: Move to Item class
+    private final Logger LOGGER = Logger.getLogger(ZireaelSword.class.getName());
+
     public ZireaelSword(JavaPlugin plugin) {
+        LOGGER.finer("ZireaelSword");
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
-//    public void equip(Player player) {
-//        equip(player, Material.DIAMOND_SWORD, ZireaelSword.class.getName());
-//    }
     public void equip(Player player) {
-        ItemStack item = new ItemStack(Material.DIAMOND_SWORD, 1);
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null) {
-            return;
-        }
-        meta.setDisplayName(ZireaelSword.class.getName()); // TODO: Simplify name
-        item.setItemMeta(meta);
-        player.getInventory().addItem(item);
-
-        ItemStack itemMana = new ItemStack(Material.BLUE_DYE, 5);
-        ItemMeta metaMana = item.getItemMeta();
-        if (metaMana == null) {
-            return;
-        }
-        metaMana.setDisplayName("Mana");
-        itemMana.setItemMeta(metaMana);
-        player.getInventory().addItem(itemMana);
-
-
+        LOGGER.finer("equip");
+        equip(player, Material.DIAMOND_SWORD, ZireaelSword.class.getName());
     }
 
     @Override
     public void effect(Player player, Action action) {
+        LOGGER.finer("effect");
         if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
             Effect.blinkForward(player, 10);
         }
