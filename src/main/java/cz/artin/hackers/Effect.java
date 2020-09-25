e82c6e1b4eb24e284f909fa30a7452c765232e78
zdenek-nemec <zdenek.nemec@artin.cz>
2020-09-25 19:24:41 +0200

    #59: Underwater blink

--- src/main/java/cz/artin/hackers/Effect.java
+++ src/main/java/cz/artin/hackers/Effect.java
@@ -6,15 +6,27 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class Effect {
+    private static final Logger LOGGER = Logger.getLogger(Effect.class.getName());
+
     public static void launchFireball(Player player) {
+        LOGGER.info("launchFireball");
         player.launchProjectile(Fireball.class);
     }
 
     public static void blinkForward(Player player, Integer distance) {
-        List<Block> lineOfSight = player.getLineOfSight(null, distance);
+        LOGGER.info("blinkForward");
+        HashSet<Material> transparentMaterials = new HashSet<Material>();
+        transparentMaterials.add(Material.AIR);
+        transparentMaterials.add(Material.CAVE_AIR);
+        transparentMaterials.add(Material.VOID_AIR);
+        transparentMaterials.add(Material.WATER);
+        List<Block> lineOfSight = player.getLineOfSight(transparentMaterials, distance);
+        LOGGER.info("blinkForward: lineOfSight length = " + lineOfSight.size());
         Location targetLocation;
         if (lineOfSight.get(lineOfSight.size() - 1).getLocation().getBlock().getType().equals(Material.AIR)) {
             targetLocation = lineOfSight.get(lineOfSight.size() - 1).getLocation();
