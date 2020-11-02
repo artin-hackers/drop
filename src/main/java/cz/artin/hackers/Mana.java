7e34f58bd11bd3df7339ed3887eccbaea3113087
zdenek-nemec <zdenek.nemec@artin.cz>
2020-11-02 21:07:35 +0100

    #63: Preparations for Mana class - Not working!

---
+++ src/main/java/cz/artin/hackers/Mana.java
@@ -0,0 +1,118 @@
+package cz.artin.hackers;
+
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Mana {
+    public void addBlackMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack mana = new ItemStack(Material.BLACK_DYE, amount);
+            player.getInventory().addItem(mana);
+
+        }
+    }
+
+    public void addBlueMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack mana = new ItemStack(Material.BLUE_DYE, amount);
+            player.getInventory().addItem(mana);
+
+        }
+    }
+
+    public void addGreenMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack mana = new ItemStack(Material.GREEN_DYE, amount);
+            player.getInventory().addItem(mana);
+
+        }
+    }
+
+    public void addRedMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack mana = new ItemStack(Material.RED_DYE, amount);
+            player.getInventory().addItem(mana);
+
+        }
+    }
+
+    public void addWhiteMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack mana = new ItemStack(Material.WHITE_DYE, amount);
+            player.getInventory().addItem(mana);
+
+        }
+    }
+
+    public boolean removeBlackMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.getInventory().containsAtLeast(new ItemStack(Material.BLACK_DYE), amount)) {
+                removeItems(player.getInventory(), Material.BLACK_DYE, amount);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeBlueMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.getInventory().containsAtLeast(new ItemStack(Material.BLUE_DYE), amount)) {
+                removeItems(player.getInventory(), Material.BLUE_DYE, amount);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeGreenMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.getInventory().containsAtLeast(new ItemStack(Material.GREEN_DYE), amount)) {
+                removeItems(player.getInventory(), Material.GREEN_DYE, amount);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeRedMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.getInventory().containsAtLeast(new ItemStack(Material.RED_DYE), amount)) {
+                removeItems(player.getInventory(), Material.RED_DYE, amount);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeWhiteMana(CommandSender sender, Integer amount) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.getInventory().containsAtLeast(new ItemStack(Material.WHITE_DYE), amount)) {
+                removeItems(player.getInventory(), Material.WHITE_DYE, amount);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/cz/artin/hackers/ZireaelSword.java
+++ src/main/java/cz/artin/hackers/ZireaelSword.java
@@ -30,4 +30,3 @@ public class ZireaelSword extends Item implements Listener {
         }
     }
 }
-
