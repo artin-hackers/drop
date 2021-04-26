8f77e5210e6d0cf63213d6bad419438454f33a6e
Filip-Bartl <fiju.bartl@gmail.com>
2021-04-26 15:32:12 +0200

    #70: bow changes

--- src/main/java/cz/artin/hackers/Bow.java
+++ src/main/java/cz/artin/hackers/Bow.java
@@ -43,7 +43,8 @@ public class Bow extends Item implements Listener {
         if (event.getEntity() instanceof Arrow) {
             LOGGER.info("Shooter: " + ((Player) event.getEntity().getShooter()).getName());
             int level = ((Player) event.getEntity().getShooter()).getLevel();
-//            setGroundFire(event.getEntity().getLocation(), level);
+//            if(ItemInMainHand = Bow)
+//            setGroundFire(event.getEntity().getLocation());
             Arrow arrow = (Arrow) event.getEntity();
             arrow.setDamage(0.25);
             arrow.setCritical(false);
